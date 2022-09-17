59e80f0b8e5eb6e016a3f83f444b8f794f0e2d3e
RubaCodes <rubacodes@gmail.com>
2022-09-17 16:18:44 +0200

    print alpha campo da gioco

--- Gioco15/Program.cs
+++ Gioco15/Program.cs
@@ -1,11 +1,16 @@
-﻿int[] campoDiGioco = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+﻿int[] campoDiGioco = {  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 };
+
 
 void printCampo(int[] array) {
-	for (int i = 0; i < campoDiGioco.Length/4; i++) {
-		Console.Write(campoDiGioco[i]);
-	}
-	Console.WriteLine();
-    Console.WriteLine("----");
+    int count = 0;
+        for (int i = 0; i < campoDiGioco.Length; i++) {
+            Console.Write(" " + campoDiGioco[i] + " ");
+            count++;
+        if (count == 4) {
+            Console.WriteLine();
+            count = 0;
+            }
+        }
 }
 
 printCampo(campoDiGioco);
