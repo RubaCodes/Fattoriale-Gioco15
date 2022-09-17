c4033439b5cb60e6df8460ddb93b5e3c9c4a67a5
RubaCodes <rubacodes@gmail.com>
2022-09-17 15:55:32 +0200

    Add project files.

---
+++ Fattoriale/Program.cs
@@ -0,0 +1,13 @@
+﻿//fattoriale :
+
+
+int fattoriale(int n) {
+    if (n == 1 || n == 0) {
+        return 1;
+    }
+    else { return n * fattoriale(n - 1); }
+}
+
+Console.WriteLine("Inserire il numero di cui fare il fattoriale");
+int numero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(fattoriale(numero));
---
+++ Gioco15/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] campoDiGioco = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+void printCampo(int[] array) {
+	for (int i = 0; i < campoDiGioco.Length/4; i++) {
+		Console.Write(campoDiGioco[i]);
+	}
+	Console.WriteLine();
+    Console.WriteLine("----");
+}
+
+printCampo(campoDiGioco);
